a382aad617c48ba5c83fb16dd57100f5aa035216
Luis Manuel Gomez <gomez.luismnl@gmail.com>
2019-11-10 19:41:11 +0100

    Adivina entre 10 y 50 ambos incluidos

--- src/LuisManuelGomezAdivino/Aciertos.java
+++ src/LuisManuelGomezAdivino/Aciertos.java
@@ -6,7 +6,7 @@ public class Aciertos {
 	
 	public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());//
+        int x = (int) (41 *Math.random())+10; // El numero a adivinar este enrte 10 y 50 ambos incluidos
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
