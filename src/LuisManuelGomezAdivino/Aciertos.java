c85ca12ad891486c3f6c8a2922ba331e7e2fa19d
Luis Manuel Gomez <gomez.luismnl@gmail.com>
2019-11-10 19:37:56 +0100

    Nueva rama cambio

--- src/LuisManuelGomezAdivino/Aciertos.java
+++ src/LuisManuelGomezAdivino/Aciertos.java
@@ -6,7 +6,7 @@ public class Aciertos {
 	
 	public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (500 *Math.random());//
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
